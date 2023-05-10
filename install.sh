d9d3a13d357a3bc00ba8ebaa4aa1d0cad6acb053
Casper Bruun <casperbruun@gmail.com>
2023-05-10 11:48:59 +0000

    feat: Install antidote ZSH plugin manager as part of the install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -eu
 
 dotfiles() {
-    git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
+    git --git-dir="${HOME}"/.dotfiles/ --work-tree="${HOME}" "$@"
 }
 
 DOTFILES_LOCATION="${HOME}"/.dotfiles
@@ -11,8 +11,13 @@ DOTFILES_LOCATION="${HOME}"/.dotfiles
 if [ ! -d "$DOTFILES_LOCATION" ]; then
     git clone --bare https://gitlab.com/cbruun/dotfiles.git "${HOME}"/.dotfiles
 else
-    asd
+    dotfiles pull
 fi
 
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
+
+. ${HOME}/.zshenv
+
+[ -d "${ZDOTDIR}"/.antidote ] && rm -rf "${ZDOTDIR}"/.antidote
+git clone --depth=1 https://github.com/mattmc3/antidote.git "${ZDOTDIR}"/.antidote
