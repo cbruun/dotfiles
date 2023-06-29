73c02db2bcbeabdc5044577af3880fbdb29a01e6
Casper Bruun <casperbruun@gmail.com>
2023-06-29 07:57:08 +0200

    feat(zsh): Add WSL specific ZSH config that is only enabled if WSL is detected

---
+++ .config/zsh/wsl.zsh
@@ -0,0 +1,13 @@
+alias op="explorer.exe"
+alias java="java.exe"
+alias dotnet="dotnet.exe"
+
+alias home="cd ~/"
+alias win="cd /mnt/c/Users/cbr"
+alias proj="win && cd projects"
+
+alias tf=terraform
+
+# Enable ssh-agent to remember passwords
+eval $(ssh-agent) &>/dev/null
+
