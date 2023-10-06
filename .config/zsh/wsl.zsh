b8674789d2e2f05e8fc6b4081079b3cd1a5e8ae9
Casper Bruun <casperbruun@gmail.com>
2023-10-06 19:19:33 +0200

    feat(zsh): Update WSL configuration

--- .config/zsh/wsl.zsh
+++ .config/zsh/wsl.zsh
@@ -1,12 +1,16 @@
-alias op="explorer.exe"
-alias java="java.exe"
-alias dotnet="dotnet.exe"
-
 alias home="cd ~/"
 alias win="cd /mnt/c/Users/cbr"
 alias proj="win && cd projects"
 
-alias tf=terraform
+alias o="explorer.exe"
+alias winget="winget.exe"
+alias java="java.exe"
+alias dotnet="dotnet.exe"
+alias op="op.exe"
 
 # Enable ssh-agent to remember passwords
 eval $(ssh-agent) &>/dev/null
+
+update_time() {
+  sudo ntpdate time.windows.com
+}
