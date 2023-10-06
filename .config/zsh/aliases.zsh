af3b249a640ef9ea7de7d0945f89e413bc570092
Casper Bruun <casperbruun@gmail.com>
2023-10-06 19:20:07 +0200

    feat(zsh): Conditionally add aliases for Terraform and Pulumi

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,4 +3,12 @@ alias zshplugins='code $ZDOTDIR/.zsh_plugins.txt'
 alias dotfiles-edit='code $HOME/projects/dotfiles'
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-alias op=xdg-open
+alias o=xdg-open
+
+if command_exists terraform; then
+  alias tf=terraform
+fi
+
+if command_exists pulumi; then
+  alias pl=pulumi
+fi
--- .config/zsh/helper-functions.zsh
+++ .config/zsh/helper-functions.zsh
@@ -1,3 +1,7 @@
 source_if_exists() {
   [[ -f "$1" ]] && source "$1"
 }
+
+command_exists() {
+  return ! COMMAND_LOCATION="$(type -p "$1")" || [[ -z $COMMAND_LOCATION ]]
+}
