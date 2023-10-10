9e1ac8e656c7cff33cb02df8e898e45ba6f58686
Casper Bruun <casperbruun@gmail.com>
2023-10-10 16:24:54 +0200

    feat(zsh): Open all ZSH files when editing the config

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,4 +1,4 @@
-alias zshconfig='code $ZDOTDIR/.zshrc'
+alias zshconfig='code $ZDOTDIR $ZDOTDIR/.zshrc'
 alias zshplugins='code $ZDOTDIR/.zsh_plugins.txt'
 alias dotfiles-edit='code $HOME/projects/dotfiles'
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
