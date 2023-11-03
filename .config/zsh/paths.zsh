9f2335c77176ffbb5292bd229ba33db704bfe91b
Casper Bruun <casperbruun@gmail.com>
2023-11-03 22:08:25 +0100

    feat(zsh): Add Cargo to PATH

--- .config/zsh/paths.zsh
+++ .config/zsh/paths.zsh
@@ -1,7 +1,6 @@
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.dotnet
 export PATH=$PATH:$HOME/.pulumi/bin
-# export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:$XDG_DATA_HOME/cargo/bin
 export PATH=$PATH:$XDG_DATA_HOME/JetBrains/Toolbox/scripts
 export PATH=$PATH:$HOME/.tfenv/bin
-
