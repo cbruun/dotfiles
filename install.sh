1824c12154013e89761ab88f77a243c269eebbcb
Casper Bruun <casperbruun@gmail.com>
2023-11-08 22:02:01 +0100

    feat: Improve install script

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 set -eu
 
-dotfiles() {
-  git --git-dir="${HOME}"/.dotfiles/ --work-tree="${HOME}" "$@"
-}
-
 DOTFILES_LOCATION="${HOME}"/.dotfiles
 
+dotfiles() {
+  git --git-dir="${DOTFILES_LOCATION}" --work-tree="${HOME}" "$@"
+}
+
 if [ ! -d "$DOTFILES_LOCATION" ]; then
-  git clone --bare https://github.com/cbruun/dotfiles.git "${HOME}"/.dotfiles
+  git clone --bare https://github.com/cbruun/dotfiles.git "$DOTFILES_LOCATION"
 else
   dotfiles pull
 fi
@@ -17,7 +17,7 @@ fi
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
 
-. ${HOME}/.zshenv
+ANTIDOTE_LOCATION="${ZDOTDIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/zsh}/.antidote"
 
-[ -d "${ZDOTDIR}"/.antidote ] && rm -rf "${ZDOTDIR}"/.antidote
-git clone --depth=1 https://github.com/mattmc3/antidote.git "${ZDOTDIR}"/.antidote
+[ -d "${ANTIDOTE_LOCATION}" ] && rm -rf "${ANTIDOTE_LOCATION}"
+git clone --depth=1 https://github.com/mattmc3/antidote.git "${ANTIDOTE_LOCATION}"
