ee10b1e4f960b2998df3c3d8a9d5e1cc41175316
Casper Bruun <casperbruun@gmail.com>
2024-03-01 20:28:58 +0100

    feat(zsh): Remove tfenv from the PATH

--- .config/zsh/base_config/paths.sh
+++ .config/zsh/base_config/paths.sh
@@ -2,7 +2,6 @@
 
 export PATH="$PATH":"$HOME"/.dotnet
 export PATH="$PATH":"$HOME"/.pulumi/bin
-export PATH="$PATH":"$HOME"/.tfenv/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/cargo/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/JetBrains/Toolbox/scripts
