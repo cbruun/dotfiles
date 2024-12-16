004b05598424c5e88a2abefe55cedeb09103d925
Casper Bruun <casperbruun@gmail.com>
2024-12-16 15:22:42 +0100

    fix(zsh): Add Cargo properly to the path

--- .config/zsh/base/paths.sh
+++ .config/zsh/base/paths.sh
@@ -4,5 +4,5 @@
 export PATH="$PATH":"$HOME"/.dotnet
 export PATH="$PATH":"$HOME"/.pulumi/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/bin
-export PATH="$PATH":"$XDG_DATA_HOME"/cargo/env
+export PATH="$PATH":"$XDG_DATA_HOME"/cargo/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/JetBrains/Toolbox/scripts
