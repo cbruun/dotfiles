d13bfb20fa4ee55d29258a9877064807b0c8d4d7
Casper Bruun <cb@zck.sh>
2025-08-19 21:02:26 +0200

    feat(mac): ensure the Mac configuration supports SSH key commit signing

--- .config/zsh/os/mac.zsh
+++ .config/zsh/os/mac.zsh
@@ -1,3 +1,15 @@
 alias o=open
 
 export PATH="$PATH":/opt/homebrew/bin
+
+local has_1p=false
+if command_exists brew && brew list --cask 1password >/dev/null 2>&1; then
+  has_1p=true
+fi
+
+# Use the same path for the 1Password agent socket across Linux and Mac.
+# This ensures `1password-ssh-agent.zsh` file works on both systems.
+if $has_1p; then
+  mkdir -p "$HOME/.1password"
+  ln -sfn "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$HOME/.1password/agent.sock"
+fi
