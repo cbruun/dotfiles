e531dad195fd0d2963e6994d191e2105de805b97
Casper Bruun <cb@zck.sh>
2025-08-19 21:07:57 +0200

    feat(mac): ensure 1Password SSH agent is configured on Mac

--- .config/zsh/apps/1password-ssh-agent.zsh
+++ .config/zsh/apps/1password-ssh-agent.zsh
@@ -1,3 +1,4 @@
-if command_exists 1password; then
-  export SSH_AUTH_SOCK=~/.1password/agent.sock
+local op_sock="$HOME/.1password/agent.sock"
+if [ -S "$op_sock" ]; then
+  export SSH_AUTH_SOCK="$op_sock"
 fi
