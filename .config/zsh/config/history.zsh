da84e6ec8d6085b46e4d14546b5506f48bc6d18d
Casper Bruun <cb@zck.sh>
2025-11-15 15:33:37 +0100

    refactor(zsh): improve zsh native history as it is still used by a plugin to provide suggestions

--- .config/zsh/config/history.zsh
+++ .config/zsh/config/history.zsh
@@ -1,8 +1,9 @@
 # Enable ZSH history for improved suggestions. See https://unix.stackexchange.com/questions/273861/unlimited-history-in-zsh
 touch "${ZDOTDIR}"/.zsh_history           # Ensure the history file exists
 export HISTFILE="$ZDOTDIR/.zsh_history"
-export HISTSIZE=2000
-export SAVEHIST=2000
+export HISTSIZE=10000
+export SAVEHIST=10000
 setopt appendhistory                      # Write to history
+setopt inc_append_history                 # Immediately append to the history file
 setopt hist_ignore_all_dups               # Delete old recorded entry if new entry is a duplicate
 setopt hist_reduce_blanks                 # Remove superfluous blanks before recording entry
