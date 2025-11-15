bb736dd8c28a0c3aa276d6fd73219b0c77937679
Casper Bruun <cb@zck.sh>
2025-11-15 14:55:28 +0100

    refactor(zsh): remove pulumi and dotnet paths, use Flox to manage project specific tool installations instead

--- .config/zsh/base/paths.sh
+++ .config/zsh/base/paths.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # Paths that must be available for all shells
-export PATH="$PATH":"$HOME"/.dotnet/tools
-export PATH="$PATH":"$HOME"/.pulumi/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/cargo/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/JetBrains/Toolbox/scripts
