141db13ec2fe79060987e418a8ae9a67c16cc32d
Casper Bruun <cb@zck.sh>
2025-11-21 12:46:13 +0100

    refactor(zsh): remove ASDF from my WSL config as it interferes with Flox

--- .config/zsh/os/wsl.zsh
+++ .config/zsh/os/wsl.zsh
@@ -18,9 +18,5 @@ update_time() {
   fi
 }
 
-export PATH="${HOME}/go/bin:$PATH"
-export ASDF_DATA_DIR="$XDG_DATA_HOME"/asdf
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-
 # Read by the nix-tools/ai-dev project
 export AI_DEV_DEFAULT_OP_PROFILE="employee"
