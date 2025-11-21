d8b9a4a59994e8c23790a04e998afe616b4a0fe4
Casper Bruun <cb@zck.sh>
2025-11-21 22:42:28 +0100

    feat(zsh): improve dotfiles update function

--- .config/zsh/config/common.zsh
+++ .config/zsh/config/common.zsh
@@ -30,8 +30,3 @@ command_exists() {
     return 1  # false
   fi
 }
-
-update_dotfiles() {
-  dotfiles pull
-  dotfiles submodule update --init --recursive --remote --depth=1
-}
--- .config/zsh/config/update.zsh
+++ .config/zsh/config/update.zsh
@@ -1,4 +1,4 @@
 update_dotfiles() {
-  dotfiles pull
-  dotfiles submodule update --init --recursive --remote --depth=1
+  dotfiles pull --recurse-submodules
+  antidote update
 }
