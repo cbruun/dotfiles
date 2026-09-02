e398a2ec71de1382dcea0916d95c0d43c63be8b8
Casper Bruun <cb@zck.sh>
2026-09-02 23:03:22 +0200

    feat(zsh): add NPM installed binaries to the path

--- .config/zsh/base/paths.sh
+++ .config/zsh/base/paths.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Paths that must be available for all shells
-export PATH="$PATH":"$XDG_DATA_HOME"/bin
-export PATH="$PATH":"$XDG_DATA_HOME"/cargo/bin
+export PATH="$PATH":"$CARGO_HOME"/bin
+export PATH="$PATH":"$NPM_CONFIG_PREFIX"/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/JetBrains/Toolbox/scripts
