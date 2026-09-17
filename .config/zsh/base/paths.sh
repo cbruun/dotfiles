9b9369f5553f4aa57c2423d59f58580389de8666
Casper Bruun <cb@zck.sh>
2026-09-17 13:01:41 +0200

    feat(zsh): add .local/bin to the PATH

--- .config/zsh/base/paths.sh
+++ .config/zsh/base/paths.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Paths that must be available for all shells
+export PATH="$HOME"/.local/bin:"$PATH"
 export PATH="$PATH":"$NIX_TOOLS_LOCATION"/ai-dev/bin
 export PATH="$PATH":"$CARGO_HOME"/bin
 export PATH="$PATH":"$NPM_CONFIG_PREFIX"/bin
