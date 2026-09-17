fc9090f73a2f6552baf73f951fc8db5a1f8993b7
Casper Bruun <cb@zck.sh>
2026-09-17 12:30:56 +0200

    feat(zsh): add nix-tools ai-dev bin to the PATH

--- .config/zsh/base/paths.sh
+++ .config/zsh/base/paths.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Paths that must be available for all shells
+export PATH="$PATH":"$NIX_TOOLS_LOCATION"/ai-dev/bin
 export PATH="$PATH":"$CARGO_HOME"/bin
 export PATH="$PATH":"$NPM_CONFIG_PREFIX"/bin
 export PATH="$PATH":"$XDG_DATA_HOME"/JetBrains/Toolbox/scripts
